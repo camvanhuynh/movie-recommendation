2729dab987e1d134f1b04f1e769e10f8df74493c
Van Cam Huynh <vancamhuynh@192-168-1-4.tpgi.com.au>
2017-08-15 01:22:06 +1000

    Fixed error with posting data to server

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,25 +24,30 @@ app.controller('viewEditController',function($scope,$http) {
   var vm = this;
   vm.isEditing = false;
 
-  $scope.postData = function (movie) {
+  $scope.postData = function () {
     console.log('function postData is invoked');
+
+    console.log('after save, scope.titele is' + $scope.title);
+
     var data = {
-      title: movie.title,
-      date: movie.date,
-      duration: movie.duration,
-      genre: movie.genre,
-      synopsis: movie.synopsis
+      title: $scope.title,
+      date: $scope.date,
+      duration: $scope.duration,
+      genre: $scope.genre,
+      synopsis: $scope.synopsis
     };
 
    // temp until we can get this working on the server
-   $scope.title = data.title;
-   $scope.date = data.date;
-   $scope.duration = data.duration;
-   $scope.genre = data.genre;
-   $scope.synopsis = data.synopsis;
+  //
+  //  $scope.title = data.title;
+  //  $scope.date = data.date;
+  //  $scope.duration = data.duration;
+  //  $scope.genre = data.genre;
+  //  $scope.synopsis = data.synopsis;
 
     console.log('started to post data');
-    $http.post('postDataResponse',JSON.stringify(data)).then(function (response) {
+    console.log('data is ' + data.title);
+    $http.post('/recommendation',JSON.stringify(data)).then(function (response) {
       console.log('response status: ' + response.status);
       console.log('status text: ' + response.statusText);
     });
@@ -52,6 +57,18 @@ app.controller('viewEditController',function($scope,$http) {
   $scope.editRecommendation = function () {
     vm.isEditing = true;
     console.log('edit movie selected');
+    console.log('before save: title is ' + $scope.title);
   };
+
+  $http.get('/recommendation').then(function (res) {
+    var object = res.data;
+    $scope.title = object.title;
+    $scope.date = object.date;
+    $scope.duration = object.duration;
+    $scope.genre = object.genre;
+    $scope.synopsis = object.synopsis;
+
+    console.log('calling http.get....');
+  })
 
 	});
--- server.js
+++ server.js
@@ -7,5 +7,22 @@ app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(3009, function() {
 
-	console.log('started listening on port 3009!!')
+	console.log('Started listening on port 3009.....')
 })
+
+var recommendation = {
+  title : "Gone with the wind",
+  year : 1951,
+  duration: 180,
+  genre : "novel movie",
+  synopsis : "Scarlette Ohara and Rheth Buller"
+};
+
+app.get('/recommendation',function (req,res) {
+	res.json(recommendation);
+});
+
+app.post('/recommendation', function (req, res) {
+	recommendation = req.body;
+	res.status(200);
+});
